e8e7b21e070d8e1f97dd9618e74b3daf9211d853
kevindo000 <70287279+kevindo000@users.noreply.github.com>
2021-03-26 20:43:41 -0500

    Score Button

---
+++ Assets/Button.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Button : MonoBehaviour
+{
+    public float time = 2;
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+        Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Basketball.prefab", typeof(GameObject));
+        GameObject clone = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
+        Vector3 pos = Vector3.zero;
+        pos.Set(-1.34f, -0.23f, 4.89f);
+        clone.transform.position = pos;
+            time = 2;
+        }
+
+    }
+
+
+}
