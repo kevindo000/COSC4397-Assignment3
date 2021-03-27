5d810b91b0efbfccd505defe3aef492fb775b4f6
kevindo000 <70287279+kevindo000@users.noreply.github.com>
2021-03-26 19:01:25 -0500

    BasketballGame

---
+++ Assets/Landed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landed : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Net")
+        {
+            Score.AddScore();
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.tag == "OutofBound")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private Text lblscore;
+    private static int scorenum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lblscore = GetComponent<Text>();
+        scorenum = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lblscore.text = "Score: " + scorenum;
+    }
+
+    public static void AddScore()
+    {
+        scorenum = 1 + scorenum;
+    }
+}
